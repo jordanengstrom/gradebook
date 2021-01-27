0b772e78e631f73a70b8dec59d933dafc4563403
Jordan Engstrom <jordan.engstrom@intimetec.com>
2021-01-26 23:04:25 -0700

    specified number of decimal points average will be displayed

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,8 +7,17 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            // var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
+            
+            double result = 0.0;
+            foreach(double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N2}");
 
             if(args.Length > 0){
                 Console.WriteLine($"Hello, {args[0]}!");
