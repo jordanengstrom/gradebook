f67fdfb569d40f8d8fb2579f2d82197e301c4475
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-01 10:21:32 -0700

    build out Book.cs class

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        private List<double> grades = new List<double>();
+        private string name;
+        public Book(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            this.grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            double result = 0.0;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MaxValue;
+
+            foreach(var number in this.grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            
+            result /= grades.Count;
+
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+
+        }
+    }
+}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,10 +7,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            Book book = new Book("Jordan's grade book");
+            book.AddGrade(95.0);
+            book.AddGrade(76);
+            book.AddGrade(56.1);
+            book.ShowStatistics();
+
             // var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-            
+
             double result = 0.0;
             foreach(double number in grades)
             {
@@ -18,13 +23,6 @@ namespace GradeBook
             }
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N2}");
-
-            if(args.Length > 0){
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
