fb76e1056f4d51c3d6a501ca32af9449f80bd730
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-03 23:06:35 -0700

    added comments to unit test file to ourline general workflow

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -8,10 +8,15 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
+            // arrange
             int x = 5;
             int y = 2;
             int expected = 10;
+
+            // act
             int actual = x * y;
+
+            // assert
             Assert.Equal(expected, actual);
         }
     }
