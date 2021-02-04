cda202e55fcb27f7b8646d4bcfeafb5ebf9d6d9e
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-03 23:05:38 -0700

    added trivial unit test

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -8,7 +8,11 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
-
+            int x = 5;
+            int y = 2;
+            int expected = 10;
+            int actual = x * y;
+            Assert.Equal(expected, actual);
         }
     }
 }
