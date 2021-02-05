a72d695f59691e08dda26ce310aa0ade64a92e21
jordanengstrom <jordan.engstrom@gmail.com>
2021-02-05 15:56:17 -0700

    refactored test class name

--- gradebook/test/GradeBook.Tests/BookTests.cs
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -3,18 +3,13 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class UnitTest1
+    public class BookTests
     {
         [Fact]
         public void Test1()
         {
             // arrange
-            int x = 5;
-            int y = 2;
-            int expected = 10;
-
-            // act
-            int actual = x * y;
+            GradeBook book = new Book   
 
             // assert
             Assert.Equal(expected, actual);
